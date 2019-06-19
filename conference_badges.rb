16989ac232946606df9f3d2e4051a47cf003dcce
Emily Asch <e.asch@me.com>
2019-06-19 01:16:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  new_array = []
   attendees.each do |attendee|
-    return "Hello, my name is #{attendee}."
+     "Hello, my name is #{attendee}."
   end 
 end 
 
