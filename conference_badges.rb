34a0e61af90ea0201cb4fc45f77044315f5c2eb2
Emily Asch <e.asch@me.com>
2019-06-19 01:14:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,13 +5,13 @@ end
 
 def batch_badge_creator(attendees)
   attendees.each do |attendee|
-    puts "Hello, my name is #{name}."
+    puts "Hello, my name is #{attendee}."
   end 
 end 
 
 def assign_rooms(attendees, room_assignments)
   attendees.each do |attendee|
-    puts ""
+    puts "Hello, #{attendee}! You'll be assigned to room #{++room_assignments}!"
   end 
   
 end 
